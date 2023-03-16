7028dabaea33245637f922e359f6c53f949671bc
Yangff <Yangff@users.noreply.github.com>
2023-03-16 04:55:56 -0500

    Update main.py
    add gaussian/z3 solve for the key directly

--- main.py
+++ main.py
@@ -5,6 +5,65 @@ import sys
 VERBOSE = False
 ExecPath = './DfX_64bit'
 NoOracle = True
+
+TIMING = False
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+import time
+
+class Timer:
+    def __init__(self):
+        self.timeReports = {}
+        self.ans = {}
+        self.reportNames = []
+        self.onGoing = {}
+        self.nowTitle = ""
+    def newSection(self, title):
+        if not TIMING:
+            return 
+        nowTime = time.time()
+        if self.nowTitle:
+            for i in self.onGoing:
+                self.timeReports[self.nowTitle][self.onGoing[i]]["stop"] = nowTime
+        self.timeReports[title] = []
+        self.nowTitle = title
+        self.onGoing = {}
+        self.reportNames.append(title)
+    def startTime(self, topic):
+        if not TIMING:
+            return 
+        nowTime = time.time()
+        if topic in self.onGoing:
+            self.timeReports[self.nowTitle][self.onGoing[topic]]["start"] = nowTime
+            self.timeReports[self.nowTitle][self.onGoing[topic]]["cnt"] += 1
+        else:
+            self.timeReports[self.nowTitle].append({"topic":topic,"start":nowTime, "cnt": 1, "sum": 0})
+            self.onGoing[topic] = len(self.timeReports[self.nowTitle]) - 1
+    def endTime(self, topic):
+        if not TIMING:
+            return 
+        nowTime = time.time()
+        st = self.timeReports[self.nowTitle][self.onGoing[topic]]['start']
+        self.timeReports[self.nowTitle][self.onGoing[topic]]["sum"] += nowTime - st
+    def writeAns(self, ans):
+        if not TIMING:
+            return
+        self.ans[self.nowTitle] = ans
+    def __str__(self):
+        if not TIMING:
+            return ""
+        res =  "Time Report\n"
+        for i in self.reportNames:
+            res += " > " + i + "\n"
+            res += self.ans[i]
+            for j in self.timeReports[i]:
+                res += "   > Topic: " + j['topic'] + "\n"
+                res += "     > Time " + str(j['sum'] / j['cnt']) + "s \n"
+        return res
+myTimer = Timer()
+
 import re
 class F:
     def __init__(self, x):
@@ -13,6 +72,7 @@ class F:
         self.name = r[0].strip()
         self.func = r[1].strip()
         self.src1 = r[2].strip()
+        self.x = x
         if not (r[4] is None):
             self.src2 = r[4].strip()
             self.srcs = [self.src1, self.src2]
@@ -27,6 +87,7 @@ def ops(x):
 
 def runAnalysis(cmd):
     # step1. init datas
+    myTimer.startTime("Netlist Process")
     cmd = list(cmd)
     mAllInputs = list(map(lambda x: re.match('INPUT\((.*)\)', x).groups()[0], filter(lambda x: x.startswith('INPUT'), cmd)))
     mOrgInput = list(filter(lambda x:x.startswith('N'), mAllInputs))
@@ -48,6 +109,8 @@ def runAnalysis(cmd):
             if not j in mxRef:
                 mxRef[j] = set()
             mxRef[j].add(mOps[i].name)
+    myTimer.endTime("Netlist Process")
+    myTimer.startTime("Finding Related Inputs")
     # step2. assist funcs
     def findUsage(ky, used, vis):
         if ky in vis:
@@ -94,23 +157,39 @@ def runAnalysis(cmd):
     key2In = {k: findRelatedInput(k) for k in mKeyInput}
     criticalInput = key2In.values()
     if (that_output == ''):
-        print('Cannot find related key of keyinputs, please check the keyinputs.')
+        eprint('Cannot find related key of keyinputs, please check the keyinputs.')
         sys.exit(2)
-    print('HD circuit datacted. That output node is \'' + that_output + '\', with a circuit size of ' + str(len(hdRelated)) + '.')
-    print('Find ' + str(len(criticalInput)) + ' HD-related inputs, should be the input bit(s) xor with key.')
+    eprint('HD circuit detacted. That output node is \'' + that_output + '\', with a circuit size of ' + str(len(hdRelated)) + '.')
+    eprint('Find ' + str(len(criticalInput)) + ' HD-related inputs, should be the input bit(s) xor with key.')
     if VERBOSE:
         for i in key2In:
-            print(i + ' xored with ' + key2In[i])
+            eprint(i + ' xored with ' + key2In[i])
 
     mSubCircuit = set()
     findSource(that_output, mSubCircuit)
-    print(that_output + ' related sub-circuit splitted, with size of ' + str(len(mSubCircuit)))
+    eprint(that_output + ' related sub-circuit splitted, with size of ' + str(len(mSubCircuit)))
+    myTimer.endTime("Finding Related Inputs")
+    """f = open('sub_circuit.bench', 'w') 
+
+    for i in mSubCircuit:
+        if (i in mOps and not i in hdRelated):
+            f.write(mOps[i].x + '\n')
+
+    f.close()
+
+    f = open('sub_circuit_withhd.bench', 'w') 
+
+    for i in mSubCircuit:
+        if (i in mOps):
+            f.write(mOps[i].x + '\n')
+
+    f.close()"""
 
     # step 3-2: find FSC2 output/so-called perturb
     # features: 
     # 1. directly used by HD-circuit
     # 2. related to criticalInput only
-
+    myTimer.startTime("Finding Perturb")
     hdRef = set()
     for i in hdRelated:
         if i in mOps:
@@ -130,9 +209,9 @@ def runAnalysis(cmd):
             perturb = i
             break
     if perturb == '':
-        print('Cannot find output of FSC circuit, maybe you can specific one.')
+        eprint('Cannot find output of FSC circuit, maybe you can specific one.')
         sys.exit(2)
-    print('Find output of FSC circuit(so called perturb): \'' + perturb + '\'. ')
+    eprint('Find output of FSC circuit(so called perturb): \'' + perturb + '\'. ')
 
     # step 3-3: check the output possibility of perturb
     # P[x = 0]
@@ -166,7 +245,9 @@ def runAnalysis(cmd):
         should_get = 1
     else:
         should_get = 0
-    print('Possibility of [\'' + perturb + '\' = 0] is ' + str(P) +', which means we need to solve \'' + perturb + '\' = ' + str(should_get))
+    eprint('Possibility of [\'' + perturb + '\' = 0] is ' + str(P) +', which means we need to solve \'' + perturb + '\' = ' + str(should_get))
+    myTimer.endTime("Finding Perturb")
+    myTimer.startTime("SAT")
     s = z3.Solver()
     z3Nodes = {}
     def allocNode(name):
@@ -192,18 +273,19 @@ def runAnalysis(cmd):
             if (op.func == 'NOR'):
                 s.add(src == z3.Not(z3.Or(res[0], res[1])))
     s.add(allocNode(perturb) == (should_get == 1))
-    print('Start sat solver...')
+    eprint('Start sat solver...')
     sat_result = s.check()
-    print('result: ' + str(sat_result))
+    eprint('result: ' + str(sat_result))
     sat_inputs = {}
     if (str(sat_result) == 'unsat'):
-        print('Something goes wrong when solving ' + perturb + ' = 1, cannot get an input')
+        eprint('Something goes wrong when solving ' + perturb + ' = 1, cannot get an input')
     else:
         m = s.model()
         sat_inputs = {i:m.evaluate(allocNode(i)) for i in criticalInput}
-    print('Found ' + str(len(sat_inputs)), 'bits of input. Test with oracle.')
+    eprint('Found ' + str(len(sat_inputs)), 'bits of input. Test with oracle.')
+    myTimer.endTime("SAT")
     if VERBOSE:
-        print(sat_inputs)
+        eprint(sat_inputs)
 
     # step 4. retrieve all bits
     
@@ -221,7 +303,7 @@ def runAnalysis(cmd):
         if x in eval_cache:
             return eval_cache[x]
         if not x in mOps:
-            print('unknown var ' + x)
+            eprint('unknown var ' + x)
             return False
         op = mOps[x]
         rs = [evalx(sat_inputs, eval_cache, keys, i) for i in op.srcs]
@@ -258,7 +340,7 @@ def runAnalysis(cmd):
     def runOracle(res):
         cmd = createOracleStr(res)
         if VERBOSE:
-            print('calling ' + " ".join(cmd))
+            eprint('calling ' + " ".join(cmd))
         return getOracle(str(check_output(cmd)))
 
     # step 4-1: check the correctness of sat
@@ -267,13 +349,14 @@ def runAnalysis(cmd):
     if not NoOracle:
         model_output = evalx(sat_inputs, eval_cache, keys, that_output)
         oracle_output = runOracle(sat_inputs)[that_output]
-        print('Model with key = 0 get a result of ' + str(model_output) + ' and Oracle returns ' + str(oracle_output == '1'))
+        eprint('Model with key = 0 get a result of ' + str(model_output) + ' and Oracle returns ' + str(oracle_output == '1'))
         if (model_output == oracle_output):
-            print('There maybe something wrong with the sat solver/ analysis or maybe key = 0')
+            eprint('There maybe something wrong with the sat solver/ analysis or maybe key = 0')
             sys.exit()
-        print('We get an input that causes the circuit having wrong result. Trying to recover the original key')
+        eprint('We get an input that causes the circuit having wrong result. Trying to recover the original key')
     else:
-        print("No oracle, try to recover key directly.")
+        eprint("No oracle, try to recover key directly.")
+
 
     def eval_nochace(res, x):
         eval_cached = {}
@@ -296,17 +379,17 @@ def runAnalysis(cmd):
         allTask = len(others)
         for i in others:
             icnt = icnt + 1
-            if VERBOSE or (icnt % 10 == 0):
-                print('Working on ' + str(icnt) + '/' + str(allTask))
+            if (VERBOSE or (icnt % 10 == 0)) and not TIMING:
+                eprint('Working on ' + str(icnt) + '/' + str(allTask))
             res[i] = not res[i]
             r1 = runOracle(res)[that_output] == '1'
             if VERBOSE:
-                print('Oracle returns ' + str(r1))
+                eprint('Oracle returns ' + str(r1))
             r2, f = eval_nochace(res, that_output)
             if (f == True):
                 flips.append(i)
             if VERBOSE:
-                print('Model returns ' + str(r2))
+                eprint('Model returns ' + str(r2))
             if (r1 == r2):
                 equset.append(i)
             else:
@@ -330,7 +413,7 @@ def runAnalysis(cmd):
         for i in others:
             icnt = icnt + 1
             if VERBOSE or (icnt % 10 == 0):
-                print('Working on ' + str(icnt) + '/' + str(allTask))
+                eprint('Working on ' + str(icnt) + '/' + str(allTask))
             res[i] = not res[i]
             r2, f = eval_nochace(res, that_output)
             if (f == True):
@@ -339,7 +422,7 @@ def runAnalysis(cmd):
             else:
                 equset.append(i)
             if VERBOSE:
-                print('Model returns ' + str(r2))
+                eprint('Model returns ' + str(r2))
             res[i] = not res[i]
         equset.append(change)
         res[change] = not res[change]
@@ -366,6 +449,8 @@ def runAnalysis(cmd):
         _att = attack_nooracle
     else:
         _att = attacker
+
+    myTimer.startTime("Solve by One PIP")
     equset, nequset, flips = _att(sat_inputs)
 
     # TypeA. flip on zero, nequset is all one
@@ -373,11 +458,11 @@ def runAnalysis(cmd):
     inpA = getFlippedInput(sat_inputs, nequset)
     rec_keysA = {i:inpA[key2In[i]] for i in key2In}
     idStrA = rec2str(rec_keysA)
-    print("Possible result A. H = " + str(numHA))
-    print("key = " + idStrA)
+    eprint("Possible result A. H = " + str(numHA))
+    eprint("key = " + idStrA)
     if VERBOSE:
-        print(nequset)
-        print(rec_keysA)
+        eprint(nequset)
+        eprint(rec_keysA)
     
 
     # TypeB, flip on one, equset + flipped is all one 
@@ -386,21 +471,58 @@ def runAnalysis(cmd):
     inpB = getFlippedInput(sat_inputs, equset)
     rec_keysB = {i:inpB[key2In[i]] for i in key2In}
     idStrB = rec2str(rec_keysB)
-    print("Possible result B. H = " + str(numHB))
-    print("key = " + idStrB)
+    eprint("Possible result B. H = " + str(numHB))
+    eprint("key = " + idStrB)
     if VERBOSE:
-        print(equset)
-        print(rec_keysB)
-        print("flips: ")
-        print(flips)
-    
+        eprint(equset)
+        eprint(rec_keysB)
+        eprint("flips: ")
+        eprint(flips)
+    myTimer.writeAns("   > H1 = " + str(numHA) + " Key1 = " + idStrA + "\n" + "   > H2 = " + str(numHB) + " Key2 = " + idStrB + '\n')
+    myTimer.endTime("Solve by One PIP")
+
+
+    minH = min(numHA, numHB)
+
+    if (minH > 8):
+        # only run gaussian for h > 8
+        # len(criticalInput) is required for solve the system
+        pips = []
+        myTimer.startTime("PIPs for Gaussian")
+        z3.set_option('smt.phase_selection',5)
+        criticalOrdered = [i for i in criticalInput]
+        for i in range(0, len(criticalOrdered)):
+            z3.set_option('smt.random_seed',5 + i)
+            sat_result = s.check()
+            sat_inputs = {}
+            m = s.model()
+            ini = [bool(m.evaluate(z3Nodes[i])) for i in criticalOrdered]
+            pips.append(ini)
+        myTimer.endTime("PIPs for Gaussian")
+        myTimer.startTime("Gaussian elimination")
+            # eprint(rec2str(map2key))
+        nowH = minH
+        factors = [list(map(lambda k: 1 if k == 0 else -1, i)) for i in pips]
+        cs = [nowH - sum(i) for i in pips]
+        # eprint(factors)
+        import numpy as np
+        a = np.array(factors)
+        b = np.array(cs)
+        x = np.linalg.solve(a, b)
+        xi = {criticalOrdered[i]: abs(x[i] - 1) < 1e-5 for i in range(0, len(x))}
+        rec_keyG = {i:xi[key2In[i]] for i in key2In}
+        eprint("key = " + rec2str(rec_keyG))
+        #print(x)
+        myTimer.endTime("Gaussian elimination")
+        myTimer.writeAns("   > H1 = " + str(numHA) + " Key1 = " + idStrA + "\n" + "   > H2 = " + str(numHB) + " Key2 = " + idStrB + '\n' + "   > KeyG = " + rec2str(rec_keyG) + "\n")
+
 import getopt
 
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hvi:e:",["ifile=", "exefile="])
+        opts, args = getopt.getopt(argv,"hvti:e:",["ifile=", "exefile="])
     except getopt.GetoptError:
         print('main.py -i <inputfile> -e <oracle path>')
         sys.exit(2)
@@ -411,23 +533,35 @@ def main(argv):
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         if opt == '-v':
-            print('Verbose log')
+            eprint('Verbose log')
             global VERBOSE
             VERBOSE = True
+        if opt == '-t':
+            global TIMING
+            TIMING = True
         if (opt in ("-e", "--exefile")):
             global ExecPath
+            global NoOracle
             ExecPath = arg
             NoOracle = False
 
     orgInputs = ''
     if (inputfile == ''):
+        myTimer.newSection("<stdin>")
         orgInputs = sys.stdin.readlines()
     else:
+        myTimer.newSection(inputfile)
         orgInputs = open(inputfile, 'r').readlines()
 
-    orgInputs = filter(lambda x: not x.startswith('#'), map(lambda x: x.strip(), orgInputs))
-
-    runAnalysis(orgInputs)
+    def run(orgInputs):
+        orgInputs = filter(lambda x: not x.startswith('#'), map(lambda x: x.strip(), orgInputs))
+        runAnalysis(orgInputs)
+    if TIMING:
+        for i in range(0, 4):
+            run(orgInputs)
+        print(myTimer)
+    else:
+        run(orgInputs)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
